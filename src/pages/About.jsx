a7096cce1767a08494236149ab3ed2c64e3a86f8
merwil vegas <com2pa@gmail.com>
2024-11-14 13:01:33 -0400

    corrigiendo detalles

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,19 +1,16 @@
 import { Flex } from '@chakra-ui/react'
 import Habilidades from './Habilidades'
 import Menu from '../layouts/Menu';
-import Footer from '../layouts/Footer'
 import Github from './Github';
 export const About = () => {
   return (
-    <>
-    
+    <>    
     <Menu/>
     <Flex flexDir='column'   gap={{base:4,md:8}} p={{base:4,md:8}} bg="" >
       {/* seccion 1 */}
       <Habilidades/>
       <Github/>
-    </Flex>
-    <Footer/>
+    </Flex>    
     </>
   )
 }
